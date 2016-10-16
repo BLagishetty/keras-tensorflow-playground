2f453a7136013e2ad768148cff73a0630b44adf1
Rohan Varma <rvarm1@gmail.com>
2016-10-16 05:18:40 -0700

    training model

--- kerasNN.py
+++ kerasNN.py
@@ -7,4 +7,11 @@ numpy.random.seed(seed)
 dataset=numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 X=dataset[:,0:8]
 Y=dataset[:,8]
-print Y
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1,init='uniform', activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X,Y,nb_epoch=150, batch_size=10)
+scores=model.evaluate(X,Y)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
